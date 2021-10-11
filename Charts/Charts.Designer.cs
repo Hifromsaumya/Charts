5501234d465bc0333344fd36a05e3db5ebd17667
Kricha Sahu <31133840+Kricha-sahu@users.noreply.github.com>
2021-10-11 12:39:58 +0530

    Add files via upload

---
+++ Charts/Charts.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Charts
+{
+    partial class Charts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblProgPercentageRAM = new MetroFramework.Controls.MetroLabel();
+            this.lblProgPercentageCPU = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarRAM = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabelRAM = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCPU = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabelCPU = new MetroFramework.Controls.MetroLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pcCPU = new System.Diagnostics.PerformanceCounter();
+            this.pcRAM = new System.Diagnostics.PerformanceCounter();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.pcCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcRAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProgPercentageRAM
+            // 
+            this.lblProgPercentageRAM.AutoSize = true;
+            this.lblProgPercentageRAM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblProgPercentageRAM.Location = new System.Drawing.Point(522, 136);
+            this.lblProgPercentageRAM.Name = "lblProgPercentageRAM";
+            this.lblProgPercentageRAM.Size = new System.Drawing.Size(29, 20);
+            this.lblProgPercentageRAM.TabIndex = 11;
+            this.lblProgPercentageRAM.Text = "0%";
+            // 
+            // lblProgPercentageCPU
+            // 
+            this.lblProgPercentageCPU.AutoSize = true;
+            this.lblProgPercentageCPU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblProgPercentageCPU.Location = new System.Drawing.Point(522, 81);
+            this.lblProgPercentageCPU.Name = "lblProgPercentageCPU";
+            this.lblProgPercentageCPU.Size = new System.Drawing.Size(29, 20);
+            this.lblProgPercentageCPU.TabIndex = 10;
+            this.lblProgPercentageCPU.Text = "0%";
+            // 
+            // metroProgressBarRAM
+            // 
+            this.metroProgressBarRAM.Location = new System.Drawing.Point(131, 136);
+            this.metroProgressBarRAM.Name = "metroProgressBarRAM";
+            this.metroProgressBarRAM.Size = new System.Drawing.Size(358, 23);
+            this.metroProgressBarRAM.TabIndex = 9;
+            // 
+            // metroLabelRAM
+            // 
+            this.metroLabelRAM.AutoSize = true;
+            this.metroLabelRAM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.metroLabelRAM.Location = new System.Drawing.Point(26, 139);
+            this.metroLabelRAM.Name = "metroLabelRAM";
+            this.metroLabelRAM.Size = new System.Drawing.Size(38, 20);
+            this.metroLabelRAM.TabIndex = 8;
+            this.metroLabelRAM.Text = "RAM";
+            // 
+            // metroProgressBarCPU
+            // 
+            this.metroProgressBarCPU.Location = new System.Drawing.Point(131, 81);
+            this.metroProgressBarCPU.Name = "metroProgressBarCPU";
+            this.metroProgressBarCPU.Size = new System.Drawing.Size(358, 23);
+            this.metroProgressBarCPU.TabIndex = 7;
+            // 
+            // metroLabelCPU
+            // 
+            this.metroLabelCPU.AutoSize = true;
+            this.metroLabelCPU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.metroLabelCPU.Location = new System.Drawing.Point(26, 84);
+            this.metroLabelCPU.Name = "metroLabelCPU";
+            this.metroLabelCPU.Size = new System.Drawing.Size(36, 20);
+            this.metroLabelCPU.TabIndex = 6;
+            this.metroLabelCPU.Text = "CPU";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            // 
+            // pcCPU
+            // 
+            this.pcCPU.CategoryName = "Processor";
+            this.pcCPU.CounterName = "% Processor Time";
+            // 
+            // pcRAM
+            // 
+            this.pcRAM.CategoryName = "Memory";
+            this.pcRAM.CounterName = "% Committed Bytes in Use";
+            // 
+            // chart
+            // 
+            this.chart.BackColor = System.Drawing.Color.LightGray;
+            this.chart.BorderlineColor = System.Drawing.Color.Black;
+            this.chart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(131, 186);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "CPU";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "RAM";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(420, 267);
+            this.chart.TabIndex = 12;
+            this.chart.Text = "chart";
+            // 
+            // Charts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.lblProgPercentageRAM);
+            this.Controls.Add(this.lblProgPercentageCPU);
+            this.Controls.Add(this.metroProgressBarRAM);
+            this.Controls.Add(this.metroLabelRAM);
+            this.Controls.Add(this.metroProgressBarCPU);
+            this.Controls.Add(this.metroLabelCPU);
+            this.Name = "Charts";
+            this.Text = "Charts";
+            ((System.ComponentModel.ISupportInitialize)(this.pcCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcRAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel lblProgPercentageRAM;
+        private MetroFramework.Controls.MetroLabel lblProgPercentageCPU;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarRAM;
+        private MetroFramework.Controls.MetroLabel metroLabelRAM;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCPU;
+        private MetroFramework.Controls.MetroLabel metroLabelCPU;
+        private System.Windows.Forms.Timer timer;
+        private System.Diagnostics.PerformanceCounter pcCPU;
+        private System.Diagnostics.PerformanceCounter pcRAM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
---
+++ Charts/Charts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Charts
+{
+    public partial class Charts : MetroFramework.Forms.MetroForm
+    {
+        public Charts()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            float fcpu = pcCPU.NextValue();
+            float fram = pcRAM.NextValue();
+            metroProgressBarCPU.Value = (int)fcpu;
+            metroProgressBarRAM.Value = (int)fram;
+            lblProgPercentageCPU.Text = string.Format("{0:0.000%}", fcpu);
+            lblProgPercentageRAM.Text = string.Format("{0:0.000%}", fram);
+            chart.Series["CPU"].Points.AddY(fcpu);
+            chart.Series["RAM"].Points.AddY(fram);
+        }
+
+        private void Charts_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
